3543569f7cdf845629523486872709fb2c54fb3d
Alexis Métaireau <alexis@notmyidea.org>
2012-09-10 19:35:59 +0200

    don't change the version of the gevent dep

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from marteau import __version__
 
 
 install_requires = ['funkload', 'pyramid',
-                    'gevent>=1.0dev',
+                    'gevent>=1.0b3',
                     'circus', 'PyYAML', 'paramiko',
                     'Mako', 'retools',
                     'virtualenv', 'Sphinx',
