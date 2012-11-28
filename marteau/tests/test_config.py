519290dd0c1c582178813eccd3215337b56c5103
Alexis Métaireau <alexis@notmyidea.org>
2012-11-28 16:28:23 +0100

    Add tests for the new config features.

---
+++ marteau/tests/test_config.py
@@ -0,0 +1,31 @@
+import os
+from unittest import TestCase
+
+from marteau.config import MarteauConfig
+from marteau.fixtures import MarteauFixture
+from marteau.tests.support import fixture, FakeTestFixture
+
+HERE = os.path.dirname(__file__)
+
+
+class TestConfig(TestCase):
+    def test_config_lookup_modules(self):
+        self.assertNotIn(FakeTestFixture, MarteauFixture._abc_registry)
+        config = MarteauConfig(
+                dict(lookup=['marteau.tests.register_fixtures', ]))
+        config.lookup_modules()
+        self.assertIn(FakeTestFixture, MarteauFixture._abc_registry)
+
+    def test_load_fixture(self):
+        with fixture(FakeTestFixture) as fix:
+            arguments = {'server': 'memcache', 'behavior': 'delay'}
+            config = MarteauConfig({'fixtures':
+                {'memcache_delay': {'class': 'FakeTestFixture',
+                                    'arguments': arguments}}})
+
+            memcache_delay = config.get_fixture('memcache_delay')
+            memcache_delay.setUp()
+            memcache_delay.tearDown()
+
+            self.assertEquals(fix.kwargs, arguments)
+            self.assertEquals(fix.config, config)
