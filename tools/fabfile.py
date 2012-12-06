14019ee574ecb5eed9cc5063d18acda52aca9185
Tarek Ziade <tarek@ziade.org>
2012-12-06 14:26:22 +0100

    simplified the script

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 
-from fabric.operations import put, sudo
+from fabric.operations import put
 from fabric.decorators import parallel, hosts
 
 from marteau.queue import Queue
@@ -24,9 +24,9 @@ def deploy_key():
             msg = 'Make sure you define an AUTH_KEY in the env.'
             raise KeyError(msg)
 
-    sudo('mkdir -p /var/loadtest')
     try:
-        put(local_path=path, remote_path='/var/loadtest/loadtest',
-            use_sudo=True, mode=0770)
+        put(local_path=path, remote_path='/home/marteau/loadtest.auth',
+            mode=0770)
     finally:
         os.remove(path)
+
